9f5f2b1cf6252a953baa1b79857780c27b2f7d8a
[APP3] 이정건 <ljg7123@daum.net>
2024-12-19 14:43:35 +0900

    feat: email로 로그인 기능 구현

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pickture/models/user_model.dart';
 
 class AuthService {
   final FirebaseAuth _firebaseAuth;
   final FirebaseFirestore _firestore;
+  UserCredential? userCredential;
 
   AuthService(this._firebaseAuth, this._firestore);
 
-  Future<UserCredential> signIn(String email, String password) => _firebaseAuth
-      .signInWithEmailAndPassword(email: email, password: password);
+  Future<UserModel> signIn(String email, String password) async {
+    userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    var snapshot = await _firestore
+        .collection('users')
+        .doc(userCredential?.user?.uid)
+        .get();
+    if (snapshot.data() == null) {
+      throw Exception('login_failed_exception');
+    }
+    return UserModel.fromJson(userCredential!.user!.uid, snapshot.data()!);
+  }
 }
