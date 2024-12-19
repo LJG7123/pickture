cdde64b545ff192edadf0e083fac49bdd124b47f
[APP3] 조진혁 <wlsgur4033@gmail.com>
2024-12-19 07:31:49 +0000

    Revert "Merge branch 'feature/create_chat_list_screen' into 'main'"
    This reverts merge request !3

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,14 @@
-import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pickture/firebase_options.dart';
 import 'package:pickture/routes.dart';
-import 'core/error/error_provider.dart';
-import 'core/error/error_widget.dart';
 
-void main() {
-  runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-
-    // Flutter 에러 핸들링
-    FlutterError.onError = (FlutterErrorDetails details) {
-      FlutterError.presentError(details);
-    };
-
-    // 플랫폼 에러 핸들링
-    PlatformDispatcher.instance.onError = (error, stack) {
-      FlutterError.presentError(
-        FlutterErrorDetails(
-          exception: error,
-          stack: stack,
-        ),
-      );
-      return true;
-    };
-
-    runApp(
-      ProviderScope(
-        observers: [AppErrorListener()],
-        child: const MyApp(),
-      ),
-    );
-  }, (error, stack) {
-    // Zone 에러 핸들링
-    debugPrint('Zone 에러: $error\n$stack');
-  });
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -49,14 +16,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GlobalErrorWidget(
-      child: MaterialApp.router(
-        routerConfig: router,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          useMaterial3: true,
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:pickture/screens/auth/sign_in/sign_in_screen.dart';
 import 'package:pickture/screens/auth/sign_up/sign_up_screen.dart';
-import 'screens/chat_list_screen.dart';
 
 final router = GoRouter(
-  initialLocation: '/chats',
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
@@ -13,10 +12,6 @@ final router = GoRouter(
     GoRoute(
       path: '/signup',
       builder: (context, state) => const SignUpScreen(),
-    ),
-    GoRoute(
-      path: '/chats',
-      builder: (context, state) => const ChatListScreen(),
     ),
   ],
 );
