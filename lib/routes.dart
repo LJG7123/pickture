ead60180d31c621ad4c7422bd56c8bddb0025674
JJH <wlsgur4033@gmail.com>
2024-12-20 13:22:49 +0900

    moved : 단순 경로 이동.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:pickture/screens/auth/sign_in/sign_in_screen.dart';
 import 'package:pickture/screens/auth/sign_up/sign_up_screen.dart';
-import 'screens/chat_list_screen.dart';
+import 'screens/chat/chat_list_screen.dart';
 
 final router = GoRouter(
   initialLocation: '/',
