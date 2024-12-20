55d81b85911088d5d180b9719f3d38548ce44e0b
[APP3] 이정건 <ljg7123@daum.net>
2024-12-20 11:53:13 +0900

    refactor: formatMessageDate() 함수를 확장함수 형태로 수정

--- lib/utils/date_util.dart
+++ lib/utils/date_util.dart
@@ -2,4 +2,24 @@ extension DateExtensions on DateTime {
   String dateOnly() {
     return "$year-$month-$day";
   }
+
+  String formatMessageDate() {
+    final now = DateTime.now();
+    final difference = now.difference(this);
+
+    if (difference.inDays == 0) {
+      // 오늘
+      return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}';
+    } else if (difference.inDays == 1) {
+      // 어제
+      return '어제';
+    } else if (difference.inDays < 7) {
+      // 이번 주
+      const weekdays = ['월', '화', '수', '목', '금', '토', '일'];
+      return weekdays[weekday - 1];
+    } else {
+      // 그 이전
+      return '$month월 $day일';
+    }
+  }
 }
