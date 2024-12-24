6efe546b4c5c3688a3ec818e0f8d352d56b0ae04
GeonHo Kim <yorjsgh@naver.com>
2024-12-24 20:17:56 +0900

    fixed : like, comment에 User 추가, 관련 dto 로직 변경, post.creatUserModel -> creator로변경경

--- lib/screens/post/post_screen.dart
+++ lib/screens/post/post_screen.dart
@@ -13,117 +13,115 @@ class PostScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final posts = ref.watch(postProvider);
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Post"),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => const SaveDialog(),
-                );
-              },
-              icon: const Icon(Icons.add),
-            )
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: (BuildContext context, int index) {
-            final post = posts[index];
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: SizedBox(
-                        height: 48,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Post"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => const SaveDialog(),
+              );
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (BuildContext context, int index) {
+          final post = posts[index];
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: SizedBox(
+                      height: 48,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(post.creator.userId),
+                          if (ref.watch(authProvider).value!.uid ==
+                              post.creator.uid)
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) =>
+                                          SaveDialog(post: post),
+                                    );
+                                  },
+                                  icon: const Icon(Icons.edit),
+                                ),
+                                IconButton(
+                                  onPressed: () => ref
+                                      .read(postProvider.notifier)
+                                      .deletePost(post),
+                                  icon: const Icon(Icons.delete),
+                                ),
+                              ],
+                            )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: 150,
+                      color: Colors.grey,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(post.createUserModel.userId),
-                            if (ref.watch(authProvider).value!.uid ==
-                                post.createUserModel.uid)
-                              Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (context) =>
-                                            SaveDialog(post: post),
-                                      );
-                                    },
-                                    icon: const Icon(Icons.edit),
-                                  ),
-                                  IconButton(
-                                    onPressed: () => ref
-                                        .read(postProvider.notifier)
-                                        .deletePost(post),
-                                    icon: const Icon(Icons.delete),
-                                  ),
-                                ],
-                              )
+                            const Text("Image or Content"),
+                            Text(post.content),
                           ],
                         ),
                       ),
                     ),
-                    Center(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: 150,
-                        color: Colors.grey,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text("Image or Content"),
-                              Text(post.content),
-                            ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          LikeButtonWidget(post: post),
+                          CommentButtonWidget(post: post),
+                          TextButton.icon(
+                            onPressed: () {},
+                            label: const Text(""),
+                            icon: const Icon(Icons.send),
                           ),
-                        ),
+                        ],
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            LikeButtonWidget(post: post),
-                            CommentButtonWidget(post: post),
-                            TextButton.icon(
-                              onPressed: () {},
-                              label: const Text(""),
-                              icon: const Icon(Icons.send),
-                            ),
-                          ],
-                        ),
-                        TextButton.icon(
-                          onPressed: () {},
-                          label: const Text(""),
-                          icon: const Icon(Icons.bookmark_border),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
-                      child: Text(
-                        "${post.createUserModel.userId} ${post.title}",
+                      TextButton.icon(
+                        onPressed: () {},
+                        label: const Text(""),
+                        icon: const Icon(Icons.bookmark_border),
                       ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                    child: Text(
+                      "${post.creator.userId} ${post.title}",
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
