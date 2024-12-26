8a9e1716d490634798a313050021b7276d3d2fb9
GeonHo Kim <yorjsgh@naver.com>
2024-12-26 21:56:46 +0900

    HOTFIX: fielProvider 누락파일 추가

---
+++ lib/providers/file_provider.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+
+final fileNotifierProvider = ChangeNotifierProvider<FileNotifier>((ref) => FileNotifier());
+
+class FileNotifier extends ChangeNotifier {
+  File? _file;
+  double _uploadProgress = 0;
+  String? _downloadUrl;
+
+  File? get file => _file;
+  double get uploadProgress => _uploadProgress;
+  String? get downloadUrl => _downloadUrl;
+
+  void setFile(File? file) {
+    _file = file;
+    notifyListeners();
+  }
+
+  void setUploadProgress(double progress) {
+    _uploadProgress = progress;
+    notifyListeners();
+  }
+
+  void setDownloadUrl(String url) {
+    _downloadUrl = url;
+    notifyListeners();
+  }
+
+  Future<void> pickFile() async {
+    final ImagePicker picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setFile(File(pickedFile.path));
+    }
+  }
+
+  Future<void> uploadFile() async {
+    final filePath = "Uploads/${DateTime.now().toIso8601String()}_${_file!.path.split('/').last}";
+    final storageRef = FirebaseStorage.instance.ref().child(filePath);
+    final uploadTask = storageRef.putFile(file!);
+    final taskSnapshot = await uploadTask;
+
+    uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
+      double progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setUploadProgress(progress);
+    });
+
+    await uploadTask.whenComplete(() {
+      setUploadProgress(100);
+    });
+
+    String url = await taskSnapshot.ref.getDownloadURL();
+    setDownloadUrl(url);
+  }
+}
